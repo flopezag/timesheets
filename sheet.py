eb111fff90aaa98bb48c901e82f79b9d4ecc7043
Fernando Lopez <fernando.lopez@fiware.org>
2021-08-31 10:22:57 +0200

    Extract information of the Projects, WPs, and Tasks

--- sheet.py
+++ sheet.py
@@ -1,14 +1,16 @@
 from googleapiclient.discovery import build
 from datetime import datetime, timedelta
+from pandas import DataFrame, Timedelta, to_timedelta
 
 
 class Timesheet:
     def __init__(self, credentials):
         # The ID and range of a sample spreadsheet.
-        self.SAMPLE_SPREADSHEET_ID = <ID>
-        self.SAMPLE_RANGE_NAME = <RANGE>
+        self.SAMPLE_SPREADSHEET_ID = "1pacXhdQ8h_Is9kEM4rNm9raTHRQ8lBAAqQ4TZYQFBgo"
+        self.SAMPLE_RANGE_NAME = "Template!B4:N"
         self.credentials = credentials
         self.values = list()
+        self.data = list()
 
     def get_data_from_timesheet(self):
         """Shows basic usage of the Sheets API.
@@ -33,7 +35,7 @@ class Timesheet:
         service.close()
         self.values = values
 
-    def data_analysis(self):
+    def data_analysis_validation(self):
         length = len(self.values)
         last_data_row = length - 74
         aux = self.values[9:last_data_row]
@@ -55,6 +57,8 @@ class Timesheet:
             # Check number hours per day equal to 8
             temp1 = list(map(lambda x: x[2], temp1))
             self.__validate_sum_hours_day__(day=i, hours=temp1, weekend=weekend)
+
+        self.data = aux
 
     def __validate_diff_hours__(self, day, hours):
         format = '%H:%M'
@@ -86,4 +90,50 @@ class Timesheet:
         if result != timedelta(hours=8) and day not in weekend:
             print(f'Error: day {day} has a sum of hours different from 8, total: {result}')
 
+    def data_extraction(self):
+        df = DataFrame(self.data)
+
+        # Assign the correct name to the columns
+        df.columns = ['day', 'code', 'project', 'wp', 'task', 'location', 'start', 'end', 'hours', 'a', 'b', 'c', 'd']
+
+        # Extract a sub-dataframe of the data interested to manage ('project', 'wp', 'task', 'hours')
+        df = df[["project", "wp", "task", "hours"]]
+
+        # Convert column "hours" from string to timedelta
+        df['hours'] = to_timedelta(df['hours'])
+
+        # Get the unique list of values of the column project
+        projects = df.project.unique().tolist()
+
+        # Calculate the sum of hours per each WP and per each task
+        for p in projects:
+            mask = df['project'].values == p
+            aux_df = df.loc[mask]
+            aux_df.columns = ["project", "wp", "task", "hours"]
+
+            # Need to check the column [1](wp) and column [2](task),
+            # - if [1] is empty then we create an array of 12 values, each per month with the sum of column [3]
+            # - if [1] has value but [2] is empty, create a list of wps in which each of them is the sum of [3]
+            # - if [1] and [2] have values, create a list of wps with a list of tasks with the array of sums of [3]
+            column1 = aux_df['wp'].values[1]
+            column2 = aux_df['task'].values[1]
+
+            if column1 == '':
+                # array of sum values for project
+                total = aux_df['hours'].sum()
+                print(f'project "{p}" total hours "{total}"')
+            elif column2 == '':
+                # array of sum values for each wp
+                wps = aux_df.wp.unique().tolist()
+                total_wps = dict()
+                for w in wps:
+                    mask_wp = aux_df['wp'].values == w
+                    aux_df_wp = aux_df.loc[mask_wp]
+                    total_wps[w] = aux_df_wp['hours'].sum()
+                    print(f'project "{p}", wp "{w}", total hours "{total_wps[w]}"')
+            else:
+                # array of sum values for each task
+                print('tbd')
+
+        print(projects)
 
